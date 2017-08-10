3997a095085590eb1f265d3db6dbebc447dac6ca
Edyta Zawada <edyta.zawada88@gmail.com>
2017-08-10 09:39:11 +0200

    javascripts correction

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ var buttons = document.getElementsByClassName('button');
 
 console.log(buttons.length);
 
-for(i=0; i<buttons.length; i++) {
-	console.log(buttons.innerText);
+for(var i=0; i<buttons.length; i++) {
+	console.log(buttons[i].innerText);
 }
+
+
